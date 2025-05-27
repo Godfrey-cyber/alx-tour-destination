5a9a633a81ee706cd515eca7c6e865e5c0b8bc26
Godfrey-cyber <godfreyndiritu26@gmail.com>
2025-05-27 13:05:03 +0300

    fixes the grid display - GuestReview

--- client/src/components/GuestReviews.jsx
+++ client/src/components/GuestReviews.jsx
@@ -10,20 +10,20 @@ const GuestReviews = () => {
 				</span>
 				<p className="text-sm">Very Good 15 · Reviews <span className="hover:text-underline text-blue-400 font-normal text-sm">Read all Reviews</span></p>
 			</div>
-			<div className="grid grid-cols gap-4">
-				<div className="col-span-4 flex flex-col p-4">
+			<div className="grid grid-cols-12 gap-4">
+				<div className="col-span-12 lg:col-span-4 rounded-md border border-gray-200 flex flex-col p-4">
 					<div className="flex items-center space-x-3">
 						<p className="text-xs font-normal">“So clean and staff were friendly. Securiry is the best. The have nice restaurant too”</p>
 						<p className="text-sm font-normal text-blue-400">Read more</p>
 					</div>
 				</div>
-				<div className="col-span-4 flex flex-col p-4">
+				<div className="col-span-12 lg:col-span-4 rounded-md border border-gray-200 flex flex-col p-4">
 					<div className="flex items-center space-x-3">
 						<p className="text-xs font-normal">“Everything! The central location, the restaurant food and the swimming pool. The close proximity to the malls and west lands Centre and the CBD is also a plus. The room is very comfortable. I would say I got value for my money. I would definately...”</p>
 						<p className="text-sm font-normal text-blue-400">Read more</p>
 					</div>
 				</div>
-				<div className="col-span-4 flex flex-col p-4">
+				<div className="col-span-12 lg:col-span-4 rounded-md border border-gray-200 flex flex-col p-4">
 					<div className="flex items-center space-x-3">
 						<p className="text-xs font-normal">“The place was really tranquil and quiet. They have a swimming pool which is very clean. They also have a restaurant in the building and the meals are affordable. The host was also very kind. Would most likely come back again and again. Asante!”</p>
 						<p className="text-sm font-normal text-blue-400">Read more</p>
