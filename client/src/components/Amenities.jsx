c680c923d43af4564611e0b97e6e35a7aaaae5c8
Godfrey-cyber <godfreyndiritu26@gmail.com>
2025-05-29 12:53:09 +0300

    fix responsiveness of the amenities componemt

--- client/src/components/Amenities.jsx
+++ client/src/components/Amenities.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Amenities = ({ destination }) => {
 	return (
-		<div className="grid grid-cols-5 gap-3 px-20 my-4">
+		<div className="grid grid-cols-1 md:grid-colss-2 grid-cols-5 gap-3 px-5 md:px-10 px-20 my-4">
 			{destination?.amenities?.map(item => (
 				<span key={item} className="flex items-center space-x-2 border border-gray-300 rounded-sm">
 					<p className="text-sm font-normal text-gray-700">item</p>
