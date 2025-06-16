c903136eeee7aaa855a22cc59ac9de7349d39415
Godfrey-cyber <godfreyndiritu26@gmail.com>
2025-06-16 10:12:16 +0300

    changed server url

--- client/src/utilities/utiles.js
+++ client/src/utilities/utiles.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export const axiosInstance = axios.create({
-	// baseURL: 'https://alx-tour-destination.onrender.com/api/v1', 
-	baseURL: 'http://localhost:5000/api/v1',
+	baseURL: 'https://alx-tour-destination.onrender.com/api/v1', 
+	// baseURL: 'http://localhost:5000/api/v1',
 	withCredentials: true, // Allows cookies (refresh token)
 });
