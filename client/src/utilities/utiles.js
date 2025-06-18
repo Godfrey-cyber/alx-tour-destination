46a61abb440b3b49b88a48794563f5ae5803658f
Godfrey-cyber <godfreyndiritu26@gmail.com>
2025-06-18 13:24:24 +0300

    changed the server url to deployed one

--- client/src/utilities/utiles.js
+++ client/src/utilities/utiles.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 export const axiosInstance = axios.create({
-	// baseURL: 'https://alx-tour-destination.onrender.com/api/v1',
-	baseURL: 'http://localhost:5000/api/v1',
+	baseURL: 'https://alx-tour-destination.onrender.com/api/v1',
+	// baseURL: 'http://localhost:5000/api/v1',
 	withCredentials: true, // Allows cookies (refresh token)
 });
 
