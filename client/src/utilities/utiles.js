b53f9844a316fb5e8833044f8b3b23b48aaa34f7
Godfrey-cyber <godfreyndiritu26@gmail.com>
2025-06-18 15:03:16 +0300

    added an extra line in the utiles.js folder

--- client/src/utilities/utiles.js
+++ client/src/utilities/utiles.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+
 export const axiosInstance = axios.create({
 	baseURL: 'https://alx-tour-destination.onrender.com/api/v1',
 	// baseURL: 'http://localhost:5000/api/v1',
