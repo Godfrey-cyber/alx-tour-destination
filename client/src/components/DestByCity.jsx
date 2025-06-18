ea30a1be11c718978527c60c0850c64e88a8cef1
Godfrey-cyber <godfreyndiritu26@gmail.com>
2025-06-18 17:40:36 +0300

    alignment

--- client/src/components/DestByCity.jsx
+++ client/src/components/DestByCity.jsx
@@ -24,47 +24,47 @@ const DestByCity = () => {
 				{!destinations
 					? <p className="text-sm font-bold text-gray-800 flex flex-col items-center justify-center w-full">No Destinations</p>
 					: destinations?.map(data => (
-							<div
-								key={data._id}
-								onClick={() =>
-									navigate(
-										`/destination/${data._id}/${data.slug}`
-									)
-								}
-								className="relative group flex flex-col space-y-4 h-96 justify-center rounded-lg rounded-lg border border-gray-300 cursor-pointer"
-							>
-								<p className="absolute left-1/2 bg-orange-500 px-6 py-1 top-0 transform -translate-x-1/2 z-50 text-sm z-50 font-bold text-white rounded-bl-2xl rounded-br-2xl">
-									{data.location.country}
+						<div
+							key={data._id}
+							onClick={() =>
+								navigate(
+									`/destination/${data._id}/${data.slug}`
+								)
+							}
+							className="relative group flex flex-col space-y-4 h-96 justify-center rounded-lg rounded-lg border border-gray-300 cursor-pointer"
+						>
+							<p className="absolute left-1/2 bg-orange-500 px-6 py-1 top-0 transform -translate-x-1/2 z-50 text-sm z-50 font-bold text-white rounded-bl-2xl rounded-br-2xl">
+								{data.location.country}
+							</p>
+							{data?.images && data.images.length > 1 && (
+								<img
+									className="rounded-tl-lg rounded-tr-lg w-full object-cover h-72"
+									src={data.images[1]}
+									alt=""
+								/>
+							)}
+							{/*<div className="overlay rounded-tl-lg rounded-tr-lg"></div>*/}
+							<div className="flex flex-col space-y-3 p-4">
+								<p className="text-sm text-gray-700 font-semibold">
+									{data.title}
 								</p>
-								{data?.images && data.images.length > 1 && (
-									<img
-										className="rounded-tl-lg rounded-tr-lg w-full object-cover h-72"
-										src={data.images[1]}
-										alt=""
-									/>
-								)}
-								{/*<div className="overlay rounded-tl-lg rounded-tr-lg"></div>*/}
-								<div className="flex flex-col space-y-3 p-4">
-									<p className="text-sm text-gray-700 font-semibold">
-										{data.title}
+								<span className="flex space-x-4 items-center justify-between">
+									<p className="text-sm text-blue-600 font-semibold">
+										{data.location.city}
 									</p>
-									<span className="flex space-x-4 items-center justify-between">
-										<p className="text-sm text-blue-600 font-semibold">
-											{data.location.city}
-										</p>
-										<p className="text-xs text-blue-600 font-semibold">
-											Show on Map
-										</p>
-									</span>
-									<p className="text-xs text-gray-700 font-normal">
-										9.3 Km from downtown
+									<p className="text-xs text-blue-600 font-semibold">
+										Show on Map
 									</p>
-									<p className="text-xs text-gray-700 font-normal">
-										{data.description}
-									</p>
-								</div>
+								</span>
+								<p className="text-xs text-gray-700 font-normal">
+									9.3 Km from downtown
+								</p>
+								<p className="text-xs text-gray-700 font-normal">
+									{data.description}
+								</p>
 							</div>
-						))}
+						</div>
+					))}
 			</div>
 		</div>
 	);
